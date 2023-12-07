997f0ef7aaac968f882880bf74d7fd25209248f0
Code12Git <108590542+Code12Git@users.noreply.github.com>
2023-12-08 01:24:36 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -23,8 +23,10 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use("/api/auth", userRoute);
 
-//routes declaration
-// app.use("/api/v1/users", userRouter);
+app.get('/', (req, res) => {
+  res.send('This route is working properly');
+});
+
 
 app.listen(PORT, () => {
   console.log("🚀 Server listening on PORT: " + PORT);
